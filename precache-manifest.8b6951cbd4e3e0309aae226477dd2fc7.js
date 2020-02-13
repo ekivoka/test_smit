eb3202a1805b74bc3f0c6ee6c60c3f7bbd0efb62
ekivoka <sonya.sesofa@gmail.com>
2020-02-13 14:35:50 +0300

    init

---
+++ precache-manifest.8b6951cbd4e3e0309aae226477dd2fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7cad71b30f4120468ca968d19e6a24",
+    "url": "/ekivoka/SMIT-test/index.html"
+  },
+  {
+    "revision": "1446ca45626861f8301e",
+    "url": "/ekivoka/SMIT-test/static/css/main.cada2df4.chunk.css"
+  },
+  {
+    "revision": "7100824ad1d29018d3b4",
+    "url": "/ekivoka/SMIT-test/static/js/2.24d87e08.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ekivoka/SMIT-test/static/js/2.24d87e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1446ca45626861f8301e",
+    "url": "/ekivoka/SMIT-test/static/js/main.c00ca870.chunk.js"
+  },
+  {
+    "revision": "e983de31e98ed5a60fa6",
+    "url": "/ekivoka/SMIT-test/static/js/runtime-main.53ef7554.js"
+  }
+]);
